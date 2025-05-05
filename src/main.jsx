7e83a0c5f17f7461f20e61eeb16042187b53efc8
hector <hector.vinas@institutvidreres.com>
2025-05-05 14:28:36 +0200

    Redirect

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate, useParams } from 'react-router-dom';
 import App from './App';
 import './index.css';
-import './i18n'; // asegúrate de tener esto
+import './i18n';
+
+const LANGUAGES = ['en', 'es', 'ca'];
+
+function LanguageValidator() {
+    const { lang } = useParams();
+
+    if (!LANGUAGES.includes(lang)) {
+        return <Navigate to="/en/home" replace />;
+    }
+
+    return <App />;
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
         <Routes>
-            <Route path="/" element={<Navigate to="/es/home" />} />
-            <Route path="/:lang/*" element={<App />} />
+            <Route path="/" element={<Navigate to="/en/home" />} />
+            <Route path="/:lang/*" element={<LanguageValidator />} />
         </Routes>
     </BrowserRouter>
 );
